79d0ec3fab638067a22998a2c8dd8592e4cc4a8e
Sklandrausis <janis.akmenskalns@gmail.com>
2020-09-16 14:33:29 +0300

    save work

--- mean_motion.py
+++ mean_motion.py
@@ -87,7 +87,7 @@ def main():
 
     plt.figure()
     ax1 = plt.subplot(111, aspect='equal')
-
+    #print(ras[2], ras[0],  ras[2] - ras[0])
     for group in groups_indexies:
         sum_of_ra_diff = []
         sum_of_dec_diff = []
@@ -97,66 +97,91 @@ def main():
         length = []
         ls_tmp = []
 
-        vel_for_group = [velocity[gi] for gi in group]
-        sum_of_vel.append(sum(vel_for_group ))
-        ls_tmp.append(sum(vel_for_group) / len(group))
+        for index in range(0, len(ras)):
+            #print(group)
+            #print(ras[index])
+            if index != 0:
+                ra_diff = [ras[index][gi] - ras[0][gi] for gi in group]
+                dec_diff = [decs[index][gi] - decs[0][gi] for gi in group]
+                sum_ra_diff = sum(ra_diff)
+                sum_dec_diff = sum(dec_diff)
 
-        for index2 in range(0, len(ras)):
-            ra_for_group = [ras[index2][gi] for gi in group]
-            sum_of_ra.append(sum(ra_for_group))
+            ra_for_group = [ras[index][gi] for gi in group]
+            sum_of_ra = sum(ra_for_group)
+            dec_for_group = [decs[index][gi] for gi in group]
+            sum_of_dec = sum(dec_for_group)
+            print(sum_of_ra)
 
-            dec_for_group = [decs[index2][gi] for gi in group]
-            sum_of_dec.append(sum(dec_for_group))
+            #print(sum(ra_diff), len(ra_diff), ra_diff)
+        print("\n\n")
+
+
+
+
+
+
+
+
+
+
+
+        '''
+
+
+            
+
+            dec_for_group = [decs[index][gi] for gi in group]
+            sum_of_dec.append(sum(dec_for_group) / len(group))
 
             ls_tmp.append(sum(ra_for_group) / len(group))
             ls_tmp.append(sum(dec_for_group) / len(group))
 
-            flux_for_group = [fluxs[index2][gi] for gi in group]
+            flux_for_group = [fluxs[index][gi] for gi in group]
             ls_tmp.append(max(flux_for_group))
 
-        for index1 in range(1, len(ras)):
-            ra_diff_tmp = [ras[index1][gi] - ras[0][gi] for gi in group]
-            sum_of_ra_diff.append(sum(ra_diff_tmp))
 
-            dec_diff_tmp = [decs[index1][gi] - decs[0][gi] for gi in group]
-            sum_of_dec_diff.append(sum(dec_diff_tmp))
 
-            length.append(np.sqrt(np.array(sum_of_ra_diff) ** 2 + np.array(sum_of_dec_diff) ** 2))
+            sum_of_ra_diff.append( sum( ra_diff_tmp ) )
 
-            ls_tmp.append(sum(ra_diff_tmp) / len(group))
-            ls_tmp.append(sum(dec_diff_tmp) / len(group))
-            ls_tmp.append(length)
-            ls_tmp.append(np.array(length) / len(group))
+
+            sum_of_dec_diff.append( sum( dec_diff_tmp ) )
+
+            length.append( np.sqrt( np.array( sum_of_ra_diff ) ** 2 + np.array( sum_of_dec_diff ) ** 2 ) )
+
+            ls_tmp.append( sum( ra_diff_tmp ) / len( group ) )
+            ls_tmp.append( sum( dec_diff_tmp ) / len( group ) )
+
+        ls_tmp.append(length)
+        ls_tmp.append(np.array(length, dtype=object) /len(group))
 
         for i in range(0, len(sum_of_ra_diff)):
-            ax1.annotate( "", xy=(sum_of_ra[i] / len( group ), sum_of_dec[i] / len( group )), xycoords='data',
+            ax1.annotate("", xy=(sum_of_ra[i] / len( group ), sum_of_dec[i] / len( group )), xycoords='data',
                           xytext=(sum_of_ra[i] / len( group ) + (20 * sum_of_ra_diff[i] / len( group )),
                                   sum_of_dec[i] / len( group ) + (20 * sum_of_dec_diff[i] / len( group ))),
-                          textcoords='data', arrowprops=dict( arrowstyle="<-", color="grey", connectionstyle="arc3" ) )
+                          textcoords='data', arrowprops=dict( arrowstyle="<-", color="grey", connectionstyle="arc3"))
 
         #plt.annotate( "", xy=(wek_x13 / k13, wek_y13 / k13), xycoords='data',
                   #xytext=(wek_x13 / k13 + (20 * xx13 / k13), wek_y13 / k13 + (20 * yy13 / k13)),
                   #textcoords='data', arrowprops=dict(arrowstyle="<-", connectionstyle="arc3" ) )
 
         ls.append(ls_tmp)
-
-
     for group_index in range(0, len(groups_indexies)):
-        ellipse = Ellipse((np.mean(ls[group_index][1]), np.mean(ls[group_index][2])), angle=0, lw=0.5, width=3*np.log10(ls[group_index][11]*1000.), height=3*np.log10(ls[group_index][11]*1000.))
+        #print(ls[group_index][9])
+        ellipse = Ellipse((ls[group_index][1], ls[group_index][2]), angle=0, lw=0.5, width=3*np.log10(ls[group_index][9]*1000.), height=3*np.log10(ls[group_index][9]*1000.))
         ax1.add_artist(ellipse)
         collor = cm.jet((ls[group_index][0] - min(velocity)) / velocity_range, 1)
         ellipse.set_facecolor(collor)
 
     # vector legend
-    plt.annotate( "", xy=(50, -150), xycoords='data', xytext=(50 + (20 * 3), -150), textcoords='data',
-              arrowprops=dict( arrowstyle="<-", connectionstyle="arc3" ) )
-    plt.text( 105, -135, "3 mas", size=8, rotation=0.0, ha="left", va="center", color='k' )
-    plt.text( 110, -170, "5.8 km s$^{-1}$", size=8, rotation=0.0, ha="left", va="center", color='k' )
+    plt.annotate("", xy=(50, -150), xycoords='data', xytext=(50 + (20 * 3), -150), textcoords='data',
+              arrowprops=dict( arrowstyle="<-", connectionstyle="arc3"))
+    plt.text(105, -135, "3 mas", size=8, rotation=0.0, ha="left", va="center", color='k')
+    plt.text(110, -170, "5.8 km s$^{-1}$", size=8, rotation=0.0, ha="left", va="center", color='k')
 
     plt.annotate( "", xy=(-60, -150), xycoords='data', xytext=(-60 + (20 * 3), -150), textcoords='data',
-              arrowprops=dict( arrowstyle="<-", color="grey", connectionstyle="arc3" ) )
-    plt.text( -5, -135, "3 mas", size=8, rotation=0.0, ha="left", va="center", color='grey' )
-    plt.text( 0, -170, "7.2 km s$^{-1}$", size=8, rotation=0.0, ha="left", va="center", color='grey' )
+              arrowprops=dict( arrowstyle="<-", color="grey", connectionstyle="arc3"))
+    plt.text(-5, -135, "3 mas", size=8, rotation=0.0, ha="left", va="center", color='grey')
+    plt.text(0, -170, "7.2 km s$^{-1}$", size=8, rotation=0.0, ha="left", va="center", color='grey')
 
     patches = []
     colors = velocity
@@ -174,6 +199,7 @@ def main():
     plt.title("G78: SW excluded", size=12)
     plt.grid(True)
     plt.show()
+    '''
     sys.exit(0)
 
 
