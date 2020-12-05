a548f55ef3682b1febd2bdac8bd0e77d5bc5a1d0
sklandrausis <janis.akmenskalns@gmail.com>
2020-12-05 16:46:43 +0200

    save work

--- g782.py
+++ g782.py
@@ -8,6 +8,20 @@ from matplotlib.patches import Circle
 from matplotlib.ticker import MultipleLocator
 import numpy as np
 
+from parsers.configparser_ import ConfigParser
+
+
+def get_configs(section, key):
+    """
+
+    :param section: configuration file secti
+    :param key: configuration file sections
+    :return: configuration file section key
+    """
+    config_file_path = "config/config.cfg"
+    config = ConfigParser(config_file_path)
+    return config.get_config(section, key)
+
 
 def main(ispec_files_dir, input_files_dir):
     rc('font', family='serif', style='normal', variant='normal', weight='normal', stretch='normal', size=12)
@@ -15,22 +29,21 @@ def main(ispec_files_dir, input_files_dir):
     minorLocatory = MultipleLocator(20)
     minorLocatorvel = MultipleLocator(1)
 
-    ispec_files = os.listdir(ispec_files_dir)
-    input_files = os.listdir(input_files_dir)
-
+    file_order = [file.strip() for file in get_configs("parameters", "fileOrder").split(",")]
+    ispec_files = []
+    input_files = []
     file_pairs = []
-    for i in range(0, len(ispec_files)):
-        for j in range(0, len(input_files)):
-            name_ispec_file = ispec_files[i].split("_")[0].lower()
-            name_input_file = input_files[j].split(".")[0].lower()
-            if name_ispec_file == name_input_file:
-                file_pairs.append((ispec_files[i], input_files[j]))
-    print(file_pairs)
+    for file in file_order:
+        ispec_files.append(file.split(".")[0].upper() + "_FRING.ISPEC")
+        input_files.append(file)
+        file_pairs.append((file.split(".")[0].upper() + "_FRING.ISPEC", file))
+
+    dates = {file.split("-")[0].strip():file.split("-")[1].strip() for file in get_configs("parameters", "dates").split(",")}
 
     fig, ax = plt.subplots(nrows=2, ncols=5, figsize=(16, 16), gridspec_kw={'height_ratios': [2, 2]})
 
     for index in range(0, len(file_pairs)):
-        title = file_pairs[index][0].split("_")[0].lower()
+        title = file_pairs[index][0].split("_")[0].upper() + "-" + dates[file_pairs[index][1].split(".")[0]]
         ispec_file = ispec_files_dir + "/" + file_pairs[index][0]
         input_file = input_files_dir + "/" + file_pairs[index][1]
 
@@ -66,16 +79,15 @@ def main(ispec_files_dir, input_files_dir):
             el.set_facecolor(c)
             rel.append([ra[i], dec[i], v1[i]])
         ax[1][index].add_artist(
-            Circle((190, -200), radius=10, angle=0, edgecolor='black', facecolor='white', alpha=0.9))
-        ax[1][index].annotate('1 Jy beam$^{-1}$', [290, -200], fontsize=12)
+            Circle((285, -200), radius=10, angle=0, edgecolor='black', facecolor='white', alpha=0.9))
+        ax[1][index].annotate('1 Jy beam$^{-1}$', [275, -200], fontsize=12)
         ax[1][index].set_aspect("equal", adjustable='box')
-        ax[1][index].set_xlim(-240, 240)
-        ax[1][index].set_ylim(-240, 240)
+        ax[1][index].set_xlim(-240, 155)
+        ax[1][index].set_ylim(-50, 345)
         ax[1][index].set_xlabel('$\\Delta$ RA (mas)', fontsize=12)
         ax[1][index].xaxis.set_minor_locator(minorLocatorx)
         ax[1][index].yaxis.set_minor_locator(minorLocatory)
         ax[1][index].invert_xaxis()
-        ax[1][index].annotate('1 Jy beam$^{-1}$', [180, -200], fontsize=12)
 
     plt.tight_layout()
     plt.subplots_adjust(top=0.97, bottom=0, wspace=0.18, hspace=0, left=0.05, right=0.99)
