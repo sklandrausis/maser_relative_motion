12c2581de316192dbd08313d53e1eb3372195e42
sklandrausis <janis.akmenskalns@gmail.com>
2021-01-08 11:08:23 +0200

    save work

--- g78m.py
+++ g78m.py
@@ -1,7 +1,6 @@
 import sys
 import argparse
 
-import matplotlib
 from matplotlib import rc, cm
 from matplotlib.ticker import MultipleLocator
 import matplotlib.pyplot as plt
@@ -33,9 +32,9 @@ def check_if_group_is_in_file(file, group):
 
 def main(group_numbers):
     rc('font', family='serif', style='normal', variant='normal', weight='normal', stretch='normal', size=12)
-    minorLocatorx = MultipleLocator(20)
-    minorLocatory = MultipleLocator(20)
-    minorLocatorvel = MultipleLocator(1)
+    minor_locatorx = MultipleLocator(20)
+    minor_locatory = MultipleLocator(20)
+    minor_locatorvel = MultipleLocator(1)
 
     file_order = [file.strip() for file in get_configs("parameters", "fileOrder").split(",")]
     input_files = []
@@ -89,20 +88,10 @@ def main(group_numbers):
             dec = np.empty(0)
             if j not in data_dict.keys():
                 velocitys = []
-                vms = []
-                vxs = []
-                dvs = []
                 intensitys = []
                 ras = []
                 decs = []
-                avgs_ra = []
-                avgs_dec = []
-                max_ra = []
-                min_ra = []
-                min_dec = []
-                max_dec = []
-                data_dict[j] = [velocitys, vms, vxs, dvs, intensitys, ras, decs,
-                                avgs_ra, avgs_dec, max_ra, min_ra, min_dec, max_dec]
+                data_dict[j] = [velocitys, intensitys, ras, decs]
 
             for i in range(0, len(channel_tmp)):
                 if group_tmp[i] == int(j):
@@ -111,81 +100,25 @@ def main(group_numbers):
                     ra = np.append(ra, ra_tmp[i])
                     dec = np.append(dec, dec_tmp[i])
 
-            if len(velocity) == 0:
-                vm = 0
-                vx = 0
-            else:
-                vm = min(velocity)
-                vx = max(velocity)
-
             data_dict[j][0].append(velocity)
-            data_dict[j][1].append(vm)
-            data_dict[j][2].append(vx)
-            data_dict[j][3].append(intensity)
-            data_dict[j][4].append(ra)
-            data_dict[j][5].append(dec)
-            if len(ra) == 0:
-                data_dict[j][6].append(0)
-                data_dict[j][7].append(0)
-                data_dict[j][8].append(0)
-                data_dict[j][9].append(0)
-                data_dict[j][10].append(0)
-                data_dict[j][11].append(0)
-            else:
-                data_dict[j][6].append(np.mean(ra))
-                data_dict[j][7].append(np.mean(dec))
-                data_dict[j][8].append(np.max(ra))
-                data_dict[j][9].append(np.min(ra))
-                data_dict[j][10].append(np.max(dec))
-                data_dict[j][11].append(np.min(dec))
-
+            data_dict[j][1].append(intensity)
+            data_dict[j][2].append(ra)
+            data_dict[j][3].append(dec)
+            if len(ra) != 0:
                 ra_max.append(np.max(ra))
                 ra_min.append(np.min(ra))
                 dec_max.append(np.max(dec))
                 dec_min.append(np.min(dec))
 
+    coord_range = max(max(ra_max) - min(ra_min), max(dec_max) - min(dec_min))
     symbols = ["o", "*", "v", "^", "<", ">", "1", "2", "3", "4"]
-
-    vm_min = []
-    vx_max = []
-    for j in group_numbers:
-        vm_min.append(min(data_dict[j][1]))
-        vx_max.append(max(data_dict[j][2]))
-
     for index in range(0, len(input_files)):
-        vms = []
-        vxs = []
-        coord_ranges = []
-        ra_maxs = []
-        ra_mins = []
-        dec_maxs = []
-        dec_mins = []
-
         for j in group_numbers:
             symbol = symbols[group_numbers.index(j)]
             velocity = data_dict[j][0][index]
-            vm = data_dict[j][1][index]
-            vms.append(vm)
-            vx = data_dict[j][2][index]
-            vxs.append(vx)
-            intensity = data_dict[j][3][index]
-            ra = data_dict[j][4][index]
-            dec = data_dict[j][5][index]
-            coord_range = max(max(data_dict[j][8]) - min(data_dict[j][9]),
-                              max(data_dict[j][10]) - min(data_dict[j][11]))
-            coord_ranges.append(coord_range)
-            if len(ra) == 0:
-                ra_maxs.append(0)
-                ra_mins.append(0)
-                dec_maxs.append(0)
-                dec_mins.append(0)
-            else:
-                ra_maxs.append(max(ra))
-                ra_mins.append(min(ra))
-                dec_maxs.append(max(dec))
-                dec_mins.append(min(dec))
-            title = input_files[index].split(".")[0].upper() + "-" + dates[input_files[index].split(".")[0]]
-            ax[0][0].set_ylabel('Flux density (Jy)', fontsize=12)
+            intensity = data_dict[j][1][index]
+            ra = data_dict[j][2][index]
+            dec = data_dict[j][3][index]
 
             for i in range(len(velocity) - 1):
                 if velocity[i] < min(v_min) or velocity[i] > max(v_max):
@@ -193,32 +126,29 @@ def main(group_numbers):
                 else:
                     c = cm.jet((velocity[i] - min(v_min)) / (max(v_max) - min(v_min)), 1)
 
-                ax[0][index].scatter((velocity[i], velocity[i + 1]), (intensity[i], intensity[i + 1]), color=c, lw=2,
-                                     marker=symbol)
-                ax[0][index].xaxis.set_minor_locator(minorLocatorvel)
-                ax[0][index].set_title(title, size=12)
-                ax[0][index].set_xlabel('$V_{\\rm LSR}$ (km s$^{-1}$)', fontsize=12)
+                ax[0][index].scatter((velocity[i], velocity[i + 1]), (intensity[i], intensity[i + 1]),
+                                     color=c, lw=2, marker=symbol)
+                ax[1][index].scatter(ra[i], dec[i], s=0.05 * coord_range * np.sqrt(intensity[i]),
+                                     color=c, lw=2, marker=symbol)
 
-                ax[1][0].set_ylabel('$\\Delta$ Dec (mas)', fontsize=12)
-                coord_range = max(max(ra_max) - min(ra_min), max(dec_max) - min(dec_min))
-                ax[1][index].scatter(ra[i], dec[i], s=0.1 *coord_range * np.sqrt(intensity[i]), color=c, lw=2, marker=symbol)
-                ax[1][index].set_aspect("equal", adjustable='box')
-                ax[1][index].set_xlabel('$\\Delta$ RA (mas)', fontsize=12)
-                ax[1][index].xaxis.set_minor_locator(minorLocatorx)
-                ax[1][index].yaxis.set_minor_locator(minorLocatory)
-                ax[1][index].invert_xaxis()
-
+        title = input_files[index].split(".")[0].upper() + "-" + dates[input_files[index].split(".")[0]]
+        ax[0][index].xaxis.set_minor_locator(minor_locatorvel)
+        ax[0][index].set_title(title, size=12)
+        ax[0][index].set_xlabel('$V_{\\rm LSR}$ (km s$^{-1}$)', fontsize=12)
+        ax[1][index].set_aspect("equal", adjustable='box')
+        ax[1][index].set_xlabel('$\\Delta$ RA (mas)', fontsize=12)
+        ax[1][index].xaxis.set_minor_locator(minor_locatorx)
+        ax[1][index].yaxis.set_minor_locator(minor_locatory)
+        ax[1][index].invert_xaxis()
         ax[0][index].set_xlim(min(v_min) - 0.5, max(v_max) + 0.5)
-        coord_range = max(max(ra_max) - min(ra_min), max(dec_max) - min(dec_min))
-
         centre = (np.mean([max(ra_max), min(ra_min)]), np.mean([max(dec_max), min(dec_min)]))
-        #ax[1][index].set_xlim(np.mean((max(ra_maxs), min(ra_mins))) - (coord_range_max * 2) - 0.5, np.mean((max(ra_maxs), min(ra_mins))) + (coord_range_max * 2) + 0.5)
-        #ax[1][index].set_ylim(np.mean((max(dec_maxs), min(dec_mins))) - (coord_range_max * 2) - 0.5, np.mean((max(dec_maxs), min(dec_mins))) + (coord_range_max * 2) + 0.5)
         ax[1][index].set_xlim(centre[0] - coord_range/2 - 10, centre[0] + coord_range/2 + 10)
         ax[1][index].set_ylim(centre[1] - coord_range/2 - 10, centre[1] + coord_range/2 + 10)
 
+    ax[0][0].set_ylabel('Flux density (Jy)', fontsize=12)
+    ax[1][0].set_ylabel('$\\Delta$ Dec (mas)', fontsize=12)
     plt.tight_layout()
-    plt.subplots_adjust(top=0.97, bottom=0, wspace=0.18, hspace=0, left=0.05, right=0.99)
+    plt.subplots_adjust(top=0.97, bottom=0, wspace=0.15, hspace=0, left=0.04, right=0.99)
     plt.show()
 
 
