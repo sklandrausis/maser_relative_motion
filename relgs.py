b4766f9ec1b2d424974c13f2cdd2105463538487
sklandrausis <janis.akmenskalns@gmail.com>
2021-02-10 13:16:23 +0200

    save work

--- relgs.py
+++ relgs.py
@@ -26,7 +26,7 @@ def str2bool(v):
 
 
 def check_if_group_is_in_file(file, group):
-    input_file = "groups/" + "/" + file
+    input_file = file
     group_nr = np.loadtxt(input_file, unpack=True, usecols=0)
 
     if group not in group_nr:
@@ -74,7 +74,7 @@ def firs_exceeds(array, value):
     return index
 
 
-def main(group_number, ddddd):
+def main(group_number, epoch, ddddd):
     matplotlib.use('TkAgg')
     configuration_items = get_configs_items()
     for key, value in configuration_items.items():
@@ -84,28 +84,11 @@ def main(group_number, ddddd):
     minor_locatory = MultipleLocator(20)
     minor_locator_level = MultipleLocator(1)
 
-    file_order = [file.strip() for file in get_configs("parameters", "fileOrder").split(",")]
-    input_files = []
+    input_file = "groups/" + epoch + ".groups"
+    date = {date.split("-")[0].strip():
+            date.split("-")[1].strip() for date in get_configs("parameters", "dates").split(",")}[epoch]
 
-    for file in file_order:
-        input_files.append(file)
-
-    dates = {file.split("-")[0].strip(): file.split("-")[1].strip() for file in
-             get_configs("parameters", "dates").split(",")}
-
-    bad_files = []
-    for file in input_files:
-        if not check_if_group_is_in_file(file.split(".")[0] + ".groups", group_number):
-            bad_files.append(file)
-            del dates[file.split(".")[0]]
-    input_files = [file for file in input_files if file not in bad_files]
-
-    data = dict()
-    max_intensity = []
-    for index in range(0, len(input_files)):
-        epoch = input_files[index].split(".")[0]
-        data[epoch] = dict()
-        input_file = "groups/" + "/" + input_files[index].split(".")[0] + ".groups"
+    if check_if_group_is_in_file(input_file, group_number):
         intensity = np.empty(0)
         channels = np.empty(0)
         ra = np.empty(0)
@@ -113,6 +96,7 @@ def main(group_number, ddddd):
         velocity = np.empty(0)
         group_tmp, channel_tmp, velocity_tmp, intensity_tmp, ra_tmp, dec_tmp = \
             np.loadtxt(input_file, unpack=True, usecols=(0, 1, 2, 3, 5, 6))
+
         for i in range(0, len(channel_tmp)):
             if group_tmp[i] == int(group_number):
                 intensity = np.append(intensity, intensity_tmp[i])
@@ -121,66 +105,30 @@ def main(group_number, ddddd):
                 dec = np.append(dec, dec_tmp[i])
                 velocity = np.append(velocity, velocity_tmp[i])
 
-        max_intensity.append(max(intensity))
-        data[epoch]["index_for_max_intensity"] = np.where(intensity == max(intensity))[0][0]
-        data[epoch]["intensity"] = intensity
-        data[epoch]["channels"] = channels
-        data[epoch]["velocity"] = velocity
-        data[epoch]["ra"] = ra
-        data[epoch]["dec"] = dec
+        max_max_intensity = max(intensity)
+        reference_index = np.where(intensity == max_max_intensity)[0][0]
+        references_ra = ra[reference_index]
+        references_dec = dec[reference_index]
+        references_velocity = velocity[reference_index]
+        print("references ra", references_ra, "references dec", references_dec,
+              "references velocity", references_velocity)
+        ra -= references_ra
+        dec -= references_dec
 
-    max_max_intensity = max(max_intensity)
-    epoch_index_for_max_intensity = max_intensity.index(max_max_intensity)
-    epochs = list(data.keys())
-    epoch_with_max_intensity = epochs[epoch_index_for_max_intensity]
-    print("epoch with max intensity", epoch_with_max_intensity)
-    reference_index = data[epoch_with_max_intensity]["index_for_max_intensity"]
-    references_ra = data[epoch_with_max_intensity]["ra"][reference_index]
-    references_dec = data[epoch_with_max_intensity]["dec"][reference_index]
-    references_velocity = data[epoch_with_max_intensity]["velocity"][reference_index]
-    print("references ra", references_ra, "references dec", references_dec, "references velocity", references_velocity)
+        fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(16, 16), dpi=90)
+        coord_range = max(max(ra) - min(ra), max(dec) - min(dec))
 
-    velocity_max = []
-    velocity_min = []
-    intensity_max = []
-    intensity_min = []
-    ra_max = []
-    ra_min = []
-    dec_max = []
-    dec_min = []
-    for epoch in data:
+        for v in range(len(velocity)):
+            if velocity[v] < min(velocity) or velocity[v] > max(velocity):
+                c = (0, 0, 0)
+            else:
+                c = cm.turbo((velocity[v] - min(velocity)) / (max(velocity) - min(velocity)), 1)
 
-        if epoch != epoch_with_max_intensity:
-            closet_velocity_index_to_references_velocity = \
-                (np.abs(data[epoch]["velocity"] - references_velocity)).argmin()
-            data[epoch]["ra"] -= data[epoch]["ra"][closet_velocity_index_to_references_velocity]
-            data[epoch]["dec"] -= data[epoch]["dec"][closet_velocity_index_to_references_velocity]
+            ax[0].scatter((velocity[v]), (intensity[v],), color=c, lw=2)
 
-        else:
-            data[epoch]["ra"] -= references_ra
-            data[epoch]["dec"] -= references_dec
-
-        velocity_max.append(max(data[epoch]["velocity"]))
-        velocity_min.append(min(data[epoch]["velocity"]))
-        intensity_max.append(max(data[epoch]["intensity"]))
-        intensity_min.append(min(data[epoch]["intensity"]))
-        ra_max.append(max(data[epoch]["ra"]))
-        ra_min.append(min(data[epoch]["ra"]))
-        dec_max.append(max(data[epoch]["dec"]))
-        dec_min.append(min(data[epoch]["dec"]))
-
-    fig, ax = plt.subplots(nrows=2, ncols=len(input_files), figsize=(16, 16), dpi=90)
-    coord_range = max(max(ra_max) - min(ra_min), max(dec_max) - min(dec_min))
-    output = []
-    output2 = []
-    for epoch in data:
-        print("epoch", epoch)
-        velocity = data[epoch]["velocity"]
-        intensity = data[epoch]["intensity"]
-        index = list(data.keys()).index(epoch)
-        ra = data[epoch]["ra"]
-        dec = data[epoch]["dec"]
-        title = dates[epoch]
+            el = Circle((ra[v], dec[v]), radius=0.05 * np.log(intensity[v] * 1000), angle=0, lw=2)
+            el.set_facecolor(c)
+            ax[1].add_artist(el)
 
         max_separation = {"r": 0, "d": -1, "separation": 0}
         sky_coords = [SkyCoord(ra[coord], dec[coord], unit=u.arcsec) for coord in range(0, len(ra))]
@@ -196,8 +144,8 @@ def main(group_number, ddddd):
         m, b = np.polyfit([ra[max_separation["r"]], ra[max_separation["d"]]],
                           [dec[max_separation["r"]], dec[max_separation["d"]]], 1)
         if ddddd:
-            ax[1][index].plot([ra[max_separation["r"]], ra[max_separation["d"]]],
-                              [m * ra[max_separation["r"]] + b, m * ra[max_separation["d"]] + b], "k--")
+            ax[1].plot([ra[max_separation["r"]], ra[max_separation["d"]]],
+                       [m * ra[max_separation["r"]] + b, m * ra[max_separation["d"]] + b], "k--")
 
         position_angle = 90 + np.degrees(np.arctan(m))
         print("position angle is ", position_angle)
@@ -277,7 +225,7 @@ def main(group_number, ddddd):
 
                         if len(coeff) == 6:
                             hist_fit = gauss2(q, *coeff)
-                            ax[0][index].plot(q, hist_fit, 'k')
+                            ax[0].plot(q, hist_fit, 'k')
 
                             print("{\\it %d} & %.3f & %.3f & %.1f & %.2f & %.2f & %.3f & %.3f & %.2f & %.2f & %.3f & "
                                   "%.1f(%.1f) & %.3f( ""%.3f)\\\\" %
@@ -288,17 +236,9 @@ def main(group_number, ddddd):
                                    max(size), max(size) * 1.64, (velocity[0] - velocity[len(velocity) - 1]) /
                                    max(size), (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64)))
 
-                            output2.append([epoch, gauss_nr, ra_tmp[gauss_nr][max_intensity_index],
-                                            dec_tmp[gauss_nr][max_intensity_index], velocity[max_intensity_index],
-                                            coeff[1], coeff[2] * 2, intensity[max_intensity_index], coeff[0],
-                                            coeff[4], coeff[5] * 2, coeff[3],
-                                            max(size), max(size) * 1.64, (velocity[0] - velocity[len(velocity) - 1]) /
-                                            max(size), (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64),
-                                            position_angle])
-
                         elif len(coeff) == 3:
                             hist_fit = gauss(q, *coeff)
-                            ax[0][index].plot(q, hist_fit, 'k')
+                            ax[0].plot(q, hist_fit, 'k')
 
                             print("{\\it %d} & %.3f & %.3f & %.1f & %.2f & %.2f & %.3f & %.3f & %.1f(%.1f) & %.3f("
                                   "%.3f)\\\\" %
@@ -308,13 +248,6 @@ def main(group_number, ddddd):
                                    max(size), max(size) * 1.64, (velocity[0] - velocity[len(velocity) - 1]) /
                                    max(size), (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64)))
 
-                            output2.append([epoch, gauss_nr, ra_tmp[gauss_nr][max_intensity_index],
-                                            dec_tmp[gauss_nr][max_intensity_index], velocity[max_intensity_index],
-                                            coeff[1], coeff[2] * 2, intensity[max_intensity_index], coeff[0],
-                                            "-", "-", "-", max(size), max(size) * 1.64,
-                                            (velocity[0] - velocity[len(velocity) - 1]) / max(size),
-                                            (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64),
-                                            position_angle])
                 else:
                     if len(size) > 0:
                         print("{\\it %d} & %.3f & %.3f & %.1f & %s & %s & %.3f & %s & %.1f(%.1f) & %.3f(%.3f)\\\\" %
@@ -324,75 +257,38 @@ def main(group_number, ddddd):
                                (velocity[0] - velocity[len(velocity) - 1]) / max(size),
                                (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64)))
 
-                        output2.append([epoch, gauss_nr, ra_tmp[gauss_nr][max_intensity_index],
-                                        dec_tmp[gauss_nr][max_intensity_index], velocity[max_intensity_index],
-                                        "-", "-", intensity[max_intensity_index], "-", "-", "-", "-", max(size),
-                                        max(size) * 1.64, (velocity[0] - velocity[len(velocity) - 1]) / max(size),
-                                        (velocity[0] - velocity[len(velocity) - 1]) / (max(size) * 1.64),
-                                        position_angle])
-
                     else:
                         print("{\\it %d} & %.3f & %.3f & %.1f & %s & %s & %.3f & %s & %s & %s\\\\" %
                               (gauss_nr, ra_tmp[gauss_nr][max_intensity_index],
                                dec_tmp[gauss_nr][max_intensity_index],
                                velocity[max_intensity_index], "-", "-", intensity[max_intensity_index], "-", "-", "-"))
 
-                        output2.append([epoch, gauss_nr, ra_tmp[gauss_nr][max_intensity_index],
-                                        dec_tmp[gauss_nr][max_intensity_index], velocity[max_intensity_index], "-",
-                                        "-", intensity[max_intensity_index], "-", "-", "-", "-", "-", "-", "-",
-                                        position_angle])
-
-        for o in range(0, len(velocity)):
-            output.append([epoch, velocity[o], intensity[o], ra[o], dec[o], position_angle])
-
-        for i in range(len(velocity)):
-            if velocity[i] < min(velocity_min) or velocity[i] > max(velocity_max):
-                c = (0, 0, 0)
-            else:
-                c = cm.turbo((velocity[i] - min(velocity_min)) / (max(velocity_max) - min(velocity_min)), 1)
-
-            ax[0][index].scatter((velocity[i]), (intensity[i],), color=c, lw=2)
-
-            el = Circle((ra[i], dec[i]), radius=0.05 * np.log(intensity[i] * 1000), angle=0, lw=2)
-            el.set_facecolor(c)
-            ax[1][index].add_artist(el)
-
-        ax[0][index].set_xlim(min(velocity_min) - 0.2, max(velocity_max) + 0.5)
-        ax[0][index].set_ylim((min(intensity_min)) - 0.5, (max(intensity_max) + 0.5))
-        ax[0][index].xaxis.set_minor_locator(minor_locator_level)
-        ax[0][index].set_title(title)
-        ax[0][index].set_xlabel('$V_{\\rm LSR}$ (km s$^{-1}$)')
-        ax[1][index].set_aspect("equal", adjustable='box')
-        ax[1][index].set_xlim(np.mean((max(ra_max), min(ra_min))) - (coord_range / 2) - 0.5,
-                              np.mean((max(ra_max), min(ra_min))) + (coord_range / 2) + 0.5)
-        ax[1][index].set_ylim(np.mean((max(dec_max), min(dec_min))) - (coord_range / 2) - 0.5,
-                              np.mean((max(dec_max), min(dec_min))) + (coord_range / 2) + 0.5)
-        ax[1][index].set_xlabel('$\\Delta$ RA (mas)')
-        ax[1][index].xaxis.set_minor_locator(minor_locatorx)
-        ax[1][index].yaxis.set_minor_locator(minor_locatory)
-        ax[1][index].invert_xaxis()
-        ax[1][index].set_yscale('linear')
-        ax[1][index].set_yscale('linear')
-
-    header1 = ["epoch", "velocity", "intensity", "ra", "dec", "position_angle"]
-    header2 = ["epoch", "gauss_nr", "ra", "dec", "velocity", "coeff1", "coeff2_*_2", "max_intensity", "coeff0", "coeff4",
-               "coeff5_*_2", "coeff3", "max_distance", "max_distance_au", "gradient", "gradient_au", "position_angle"]
-
-    np.savetxt("cloudlet_" + str(group_number) + "._coords.csv", np.array(output, dtype=object), delimiter=", ", fmt='%s',
-               header=",".join(header1))
-    np.savetxt("cloudlet_" + str(group_number) + "._sats.csv", np.array(output2, dtype=object), delimiter=", ", fmt='%s',
-               header=",".join(header2))
-    ax[0][0].set_ylabel('Flux density (Jy)')
-    ax[1][0].set_ylabel('$\\Delta$ Dec (mas)')
-    plt.tight_layout()
-    plt.subplots_adjust(top=0.947, bottom=0.085, left=0.044, right=0.987, hspace=0.229, wspace=0.182)
-    plt.show()
+        ax[0].set_xlim(min(velocity) - 0.2, max(velocity) + 0.5)
+        ax[0].set_ylim((min(intensity)) - 0.5, (max(intensity) + 0.5))
+        ax[0].xaxis.set_minor_locator(minor_locator_level)
+        ax[0].set_title(date)
+        ax[1].set_aspect("equal", adjustable='box')
+        ax[1].set_xlim(np.mean((max(ra), min(ra))) - (coord_range / 2) - 0.5,
+                              np.mean((max(ra), min(ra))) + (coord_range / 2) + 0.5)
+        ax[1].set_ylim(np.mean((max(dec), min(dec))) - (coord_range / 2) - 0.5,
+                              np.mean((max(dec), min(dec))) + (coord_range / 2) + 0.5)
+        ax[0].set_ylabel('Flux density (Jy)')
+        ax[1].set_ylabel('$\\Delta$ Dec (mas)')
+        ax[0].set_xlabel('$V_{\\rm LSR}$ (km s$^{-1}$)')
+        ax[1].set_xlabel('$\\Delta$ RA (mas)')
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.947, bottom=0.085, left=0.044, right=0.987, hspace=0.229, wspace=0.182)
+        plt.show()
+    else:
+        print("group is not in epoch")
+        sys.exit()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='plot group')
     parser.add_argument('group_number', type=int, help='group number')
+    parser.add_argument('epoch', type=str, help='epoch name', choices=["el032", "em064c", "em064d", "es066e", "ea063"])
     parser.add_argument('--d', type=str2bool, help='plot line', default=True)
     args = parser.parse_args()
-    main(args.group_number, args.d)
+    main(args.group_number, args.epoch, args.d)
     sys.exit(0)
